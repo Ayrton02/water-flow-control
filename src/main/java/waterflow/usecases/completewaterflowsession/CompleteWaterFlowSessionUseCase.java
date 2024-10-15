f46ae69cd19a13d2085ece28c95d6930fbdecbcc
Ayrton <ayrton.0210@gmail.com>
2024-10-14 21:37:51 -0300

    feat: add complete water flow session

---
+++ src/main/java/waterflow/usecases/completewaterflowsession/CompleteWaterFlowSessionUseCase.java
@@ -0,0 +1,24 @@
+package waterflow.usecases.completewaterflowsession;
+
+import core.valueobjects.ID;
+import waterflow.domain.entities.WaterFlowSession;
+import waterflow.domain.exceptions.WaterSessionNotFoundException;
+
+public class CompleteWaterFlowSessionUseCase {
+
+    private CompleteWaterFlowSessionRepository repository;
+
+    CompleteWaterFlowSessionUseCase(CompleteWaterFlowSessionRepository repository) {
+        this.repository = repository;
+    }
+
+    public void execute(ID id) {
+        WaterFlowSession session = this.repository.findById(id);
+        if (session == null) {
+            throw new WaterSessionNotFoundException();
+        }
+
+        session.complete();
+        this.repository.save(session, session.getWaterPump());
+    }
+}
---
+++ src/test/java/waterflow/usecases/CompleteWaterFlowSessionUseCaseTest.java
@@ -0,0 +1,69 @@
+package waterflow.usecases;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import waterflow.domain.entities.LiterWaterContainer;
+import waterflow.domain.entities.LiterWaterPump;
+import waterflow.domain.entities.LiterWaterSource;
+import waterflow.domain.entities.WaterFlowSession;
+import waterflow.domain.valueobjects.Liter;
+import waterflow.domain.valueobjects.LiterFlow;
+import waterflow.usecases.completewaterflowsession.CompleteWaterFlowSessionRepository;
+import waterflow.usecases.completewaterflowsession.CompleteWaterFlowSessionUseCase;
+
+import java.util.concurrent.TimeUnit;
+
+public class CompleteWaterFlowSessionUseCaseTest {
+
+    private AutoCloseable closeable;
+
+    @InjectMocks()
+    private CompleteWaterFlowSessionUseCase usecase;
+
+    @Mock()
+    private CompleteWaterFlowSessionRepository repository;
+
+    @Before()
+    public void setup() {
+        this.closeable = MockitoAnnotations.openMocks(this);
+    }
+
+    @After()
+    public void after() throws Exception {
+        closeable.close();
+    }
+
+    @Test()
+    public void shouldCompleteWaterFlowSession() {
+        LiterWaterContainer container = new LiterWaterContainer(
+                new Liter(100d),
+                new Liter(0d)
+        );
+        LiterWaterSource source = new LiterWaterSource(
+                new Liter(100d),
+                new Liter(0d),
+                new Liter(100d)
+        );
+        LiterFlow literFlow = new LiterFlow(
+                new Liter(1d),
+                TimeUnit.SECONDS
+        );
+        LiterWaterPump pump = new LiterWaterPump(
+                literFlow
+        );
+
+        WaterFlowSession session = WaterFlowSession.create(source, container, pump);
+        session.start();
+
+        Mockito.when(repository.findById(session.getId())).thenAnswer(a -> session);
+
+        this.usecase.execute(session.getId());
+        Assert.assertEquals(session.getStatus(), "COMPLETED");
+    }
+}
