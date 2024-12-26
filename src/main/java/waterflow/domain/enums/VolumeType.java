17f254ae3034aa49e264f83ac24dcbdde40f0978
Ayrton <ayrton.0210@gmail.com>
2024-12-26 16:54:13 -0300

    feat: add volume type fromString method

--- src/main/java/waterflow/domain/enums/VolumeType.java
+++ src/main/java/waterflow/domain/enums/VolumeType.java
@@ -1,5 +1,11 @@
 package waterflow.domain.enums;
 
 public enum VolumeType {
-    LITER
+    LITER;
+    public static VolumeType fromString(String any) {
+      if (any.equalsIgnoreCase("LITER")) {
+        return VolumeType.LITER;
+      }
+      return null;
+    }
 }
